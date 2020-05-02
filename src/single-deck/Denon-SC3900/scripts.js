05ca5da36a15da0477336aa9cb9769226ca709b4
Nicolas MURE <nicolas.mure2019@gmail.com>
2020-05-02 21:08:33 +0200

    be able to clear hotcues
    Handle CLR button behavior in order to clear a hotcue.
    
    - on a hotcue button press : set the hotcue or go back to it if already
      set.
    - on a hotcue button press while the CLR button is pressed down : clear
      the pressed hotcue.

--- src/single-deck/Denon-SC3900/scripts.js
+++ src/single-deck/Denon-SC3900/scripts.js
@@ -4,8 +4,6 @@ function DenonSC3900 () {}
 
 // @see Denon manual
 var LIGHT_ON = 0x4A
-var LIGHT_OFF = 0x4B
-var LIGHT_BLINK = 0x4C
 
 // @see Denon manual
 var HOTCUES_WRITE_ADDRESSES = {
@@ -44,11 +42,39 @@ var HOTCUES_WRITE_ADDRESSES = {
 }
 
 /**
+ * Constructor for a registry which would indicate whether the CLR button is
+ * being held down or not.
+ */
+function createClrButtonRegistry () {
+    var registry = {};
+
+    var isPresentInRegistry = function (group) {
+        return undefined !== registry[group]
+    }
+
+    return {
+        buttonPressed: function (group) {
+            registry[group] = true;
+        },
+        buttonReleased: function (group) {
+            if (isPresentInRegistry(group)) {
+                delete registry[group];
+            }
+        },
+        isPressed: function (group) {
+            return isPresentInRegistry(group);
+        }
+    }
+}
+
+var clrButtonRegistry = createClrButtonRegistry()
+
+/**
  * @param number inputChannel (0 based)
  *
  * @return string
  */
-DenonSC3900.getGroup = function (inputChannel) {
+function getGroup (inputChannel) {
     var channelNumber = inputChannel + 1;
 
     return "[Channel" + channelNumber + "]";
@@ -59,7 +85,7 @@ DenonSC3900.getGroup = function (inputChannel) {
  *
  * @return number (hex)
  */
-DenonSC3900.getOutputMidiChannel = function (inputChannel) {
+function getOutputMidiChannel (inputChannel) {
     // As specified in the Denon manual : signals must be sent on 0xBn where
     // n is the channel number [0-15].
     return 0xB0 + inputChannel;
@@ -69,11 +95,11 @@ DenonSC3900.getOutputMidiChannel = function (inputChannel) {
  * @param number outputChannel
  * @param string group
  */
-DenonSC3900.renderHotcuesLights = function (outputChannel, group) {
+function renderHotcuesLights (outputChannel, group) {
     for (var i = 1; i < 9; i++) {
         var settingName = "hotcue_" + i + "_position";
 
-        var isHotcueSet = -1 != engine.getValue(group, settingName)
+        var isHotcueSet = -1 !== engine.getValue(group, settingName)
 
         var destinationAddress = isHotcueSet
             ? HOTCUES_WRITE_ADDRESSES[i]["light"]
@@ -85,12 +111,49 @@ DenonSC3900.renderHotcuesLights = function (outputChannel, group) {
 }
 
 // on MIDIBANK switch
-DenonSC3900.midiBankSwitch = function (channel, control, value, status) {
+DenonSC3900.midiBankSwitch = function (channel) {
     // There is no way to know if the MIDIBANK2 is enabled on the Denon unit,
     // so we render the 8 hotcues lights to cover the two cases
     // (MIDIBANK2 enabled/disabled).
-    DenonSC3900.renderHotcuesLights(
-        DenonSC3900.getOutputMidiChannel(channel),
-        DenonSC3900.getGroup(channel)
+    renderHotcuesLights(
+        getOutputMidiChannel(channel),
+        getGroup(channel)
     )
+}
+
+// on CLR button press
+DenonSC3900.clrButtonPress = function (channel) {
+    clrButtonRegistry.buttonPressed(getGroup(channel))
+}
+
+// on CLR button release
+DenonSC3900.clrButtonRelease = function (channel) {
+    clrButtonRegistry.buttonReleased(getGroup(channel))
+}
+
+// on a hotcue press
+function hotcuePress (inputChannel, hotcueNumber) {
+    var group = getGroup(inputChannel)
+
+    var action = clrButtonRegistry.isPressed(group)
+        ? "clear"
+        : "activate"
+    ;
+
+    var valueName = "hotcue_" + hotcueNumber + "_" + action;
+
+    engine.setValue(group, valueName, true);
+}
+
+function createHotcuePressHandler (hotcueNumber) {
+    return function (channel) {
+        hotcuePress(channel, hotcueNumber)
+    }
+}
+
+for (var i = 1; i < 9; i++) {
+    var hotcuePressHandlerName = "hotcue" + i + "Press"
+
+    // create hotcueXPress handlers
+    DenonSC3900[hotcuePressHandlerName] = createHotcuePressHandler(i)
 }
