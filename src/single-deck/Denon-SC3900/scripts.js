b88fd3cc9036c10b5d013fda5f70b2884a4405d5
Nicolas MURE <nicolas.mure2019@gmail.com>
2020-05-02 19:09:39 +0200

    handle MIDIBANK navigation
    properly render the hotcues lights on the SC3900 unit when switching the
    MIDIBANK.

--- src/single-deck/Denon-SC3900/scripts.js
+++ src/single-deck/Denon-SC3900/scripts.js
@@ -2,8 +2,60 @@
 
 function DenonSC3900 () {}
 
+// @see Denon manual
+var LIGHT_ON = 0x4A
+var LIGHT_OFF = 0x4B
+var LIGHT_BLINK = 0x4C
+
+// @see Denon manual
+var HOTCUES_WRITE_ADDRESSES = {
+    1: {
+        light: 0x11,
+        dimmer: 0x12
+    },
+    2: {
+        light: 0x13,
+        dimmer: 0x14
+    },
+    3: {
+        light: 0x15,
+        dimmer: 0x16
+    },
+    4: {
+        light: 0x17,
+        dimmer: 0x18
+    },
+    5: {
+        light: 0x31,
+        dimmer: 0x32
+    },
+    6: {
+        light: 0x33,
+        dimmer: 0x34
+    },
+    7: {
+        light: 0x35,
+        dimmer: 0x36
+    },
+    8: {
+        light: 0x37,
+        dimmer: 0x38
+    }
+}
+
 /**
- * @param number inputChannel 0 based.
+ * @param number inputChannel (0 based)
+ *
+ * @return string
+ */
+DenonSC3900.getGroup = function (inputChannel) {
+    var channelNumber = inputChannel + 1;
+
+    return "[Channel" + channelNumber + "]";
+}
+
+/**
+ * @param number inputChannel (0 based)
  *
  * @return number (hex)
  */
@@ -13,23 +65,31 @@ DenonSC3900.getOutputMidiChannel = function (inputChannel) {
     return 0xB0 + inputChannel;
 }
 
-DenonSC3900.renderHotcuesLights = function (inputChannel) {
-    var outputChannel = DenonSC3900.getOutputMidiChannel(inputChannel)
+/**
+ * @param number outputChannel
+ * @param string group
+ */
+DenonSC3900.renderHotcuesLights = function (outputChannel, group) {
+    for (var i = 1; i < 9; i++) {
+        var settingName = "hotcue_" + i + "_position";
 
-    // light on all hotcues dimmer lights
-    midi.sendShortMsg(outputChannel, 0x4A, 0x12) // light on hotcue 1 dummer light
-    midi.sendShortMsg(outputChannel, 0x4A, 0x14) // light on hotcue 2 dummer light
-    midi.sendShortMsg(outputChannel, 0x4A, 0x16) // light on hotcue 3 dummer light
-    midi.sendShortMsg(outputChannel, 0x4A, 0x18) // light on hotcue 4 dummer light
-    midi.sendShortMsg(outputChannel, 0x4A, 0x32) // light on hotcue 5 dummer light
-    midi.sendShortMsg(outputChannel, 0x4A, 0x34) // light on hotcue 6 dummer light
-    midi.sendShortMsg(outputChannel, 0x4A, 0x36) // light on hotcue 7 dummer light
-    midi.sendShortMsg(outputChannel, 0x4A, 0x38) // light on hotcue 8 dummer light
+        var isHotcueSet = -1 != engine.getValue(group, settingName)
+
+        var destinationAddress = isHotcueSet
+            ? HOTCUES_WRITE_ADDRESSES[i]["light"]
+            : HOTCUES_WRITE_ADDRESSES[i]["dimmer"]
+        ;
+
+        midi.sendShortMsg(outputChannel, LIGHT_ON, destinationAddress);
+    }
 }
 
 DenonSC3900.midiBankSwitch = function (channel, control, value, status) {
     // There is no way to know if the MIDIBANK2 is enabled on the denon unit,
     // so we light all the dimmers to cover the two cases
     // (MIDIBANK2 enabled/disabled).
-    DenonSC3900.renderHotcuesLights(channel)
+    DenonSC3900.renderHotcuesLights(
+        DenonSC3900.getOutputMidiChannel(channel),
+        DenonSC3900.getGroup(channel)
+    )
 }
